2329b410341e70579b1c0fb50bc5177c9be5c3ea
tsuter20 <34869731+tsuter20@users.noreply.github.com>
2019-04-18 14:47:56 -0600

    Add files via upload

--- TrevorSuter.js
+++ TrevorSuter.js
@@ -1,6 +1,6 @@
-var quoteArray = ['Never regret anything that made you smile. - Mark Twain', 'Everything you can imagine is real. - Pablo Picasso', 'What we think, we become. - Buddha', 'All limitations are self-imposed. - Oliver Wendall Holmes', 'Oh, the things you can find, if you don\'t stay behind. - Dr. Seuss', 'Be so good they can\'t ignore you. - Steve Martin', 'Strive for greatness. - LeBron James']
+var quoteArray = ['Never regret anything that made you smile. - Mark Twain', 'Everything you can imagine is real. - Pablo Picasso', 'What we think, we become. - Buddha', 'All limitations are self-imposed. - Oliver Wendall Holmes', 'Oh, the things you can find, if you don\'t stay behind. - Dr. Seuss', 'Be so good they can\'t ignore you. - Steve Martin', 'Strive for greatness. - LeBron James', 'Do one thing every day that scares you. - Eleanor Roosevelt']
 
 function newQuote() {
     var randomNumber = Math.floor(Math.random() * (quoteArray.length));
-    document.getElementById('quoteGenerator').innerHTML = quoteArray[randomNumber];
+    document.getElementById('quote').innerHTML = quoteArray[randomNumber];
 }
